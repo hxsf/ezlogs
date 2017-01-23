8dd5e51229049763a3ade9c1fbb4b1b23a80d60d
hxsf <hxsf@vip.qq.com>
2017-01-23 10:27:29 +0800

    add date file driver

--- test/file-date.js
+++ test/file-date.js
@@ -7,11 +7,18 @@ const logger = require('../index')([{
     max_backup: 5
 }])
 logger.trace('abcd')
-let i = 0
-var clock = setInterval(()=>{
-    logger.fatal('log', i++)
-}, 100)
-setTimeout(() => {
-    logger.warn('end')
-    clearInterval(clock)
-}, 12000)
+// let i = 0
+// var clock = setInterval(()=>{
+//     logger.fatal('log', i++)
+// }, 100)
+console.time('a')
+console.time('b')
+for (var i = 0; i < 300000; i++) {
+    logger.fatal('log', i)
+}
+console.timeEnd('a')
+process.on('exit', ()=> console.timeEnd('b'))
+// setTimeout(() => {
+//     logger.warn('end')
+//     clearInterval(clock)
+// }, 12000)
